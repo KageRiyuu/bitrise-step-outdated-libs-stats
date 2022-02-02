06d8a6acf04f9210a4f6ec3885f000811c73dbca
Riyuu, Kage | Ryu | ECMPD <bilal.a.ahmad@rakuten.com>
2022-02-02 10:36:43 +0900

    added dummy code

--- step.sh
+++ step.sh
@@ -57,7 +57,36 @@ function markOldPods() {
 }
 
 function generatePodsStats() {
-    local OUTPUT=$(bundle exec pod outdated)
+    # local OUTPUT=$(bundle exec pod outdated)
+    local OUTPUT="The following pod updates are available:
+- DateToolsSwift 4.0.0 -> 4.0.0 (latest version 5.0.0)
+- ECKit-Utils 1.2.2 -> 1.2.2 (latest version 3.1.0)
+- ECNetworking 0.8.1 -> 0.8.1 (latest version 2.0.1)
+- Firebase 8.0.0 -> (unused) (latest version 8.11.0)
+- FirebaseABTesting 8.8.0 -> 8.11.0 (latest version 8.11.0)
+- FirebaseAnalytics 8.0.0 -> 8.0.0 (latest version 8.11.0)
+- FirebaseCore 8.0.0 -> 8.0.0 (latest version 8.11.0)
+- FirebaseCoreDiagnostics 8.8.0 -> 8.11.0 (latest version 8.11.0)
+- FirebaseCrashlytics 8.0.0 -> 8.0.0 (latest version 8.11.0)
+- FirebaseInstallations 8.8.0 -> 8.11.0 (latest version 8.11.0)
+- FirebasePerformance 8.0.0 -> 8.0.0 (latest version 8.11.0)
+- FirebaseRemoteConfig 8.0.0 -> 8.0.0 (latest version 8.11.0)
+- GoogleAppMeasurement 8.0.0 -> 8.0.0 (latest version 8.11.0)
+- GoogleDataTransport 9.1.0 -> 9.1.2 (latest version 9.1.2)
+- GoogleUtilities 7.5.2 -> (unused) (latest version 7.7.0)
+- KeychainAccess 3.1.1 -> 3.1.1 (latest version 4.2.2)
+- OHHTTPStubs 6.1.0 -> (unused) (latest version 9.1.0)
+- PromisesObjC 1.2.12 -> 1.2.12 (latest version 2.0.0)
+- Protobuf 3.18.0 -> 3.19.4 (latest version 4.0.0-rc1)
+- RAnalytics 5.3.0 -> 5.3.0 (latest version 9.1.1)
+- RInAppMessaging 4.0.1 -> 4.0.1 (latest version 6.0.0)
+- RLogger 1.2.1 -> 1.2.1 (latest version 2.0.0)
+- RPushPNP 2.1.0 -> 2.1.0 (latest version 5.1.0)
+- SDWebImage 5.0.2 -> 5.0.2 (latest version 5.12.3)
+- SnapshotTesting 1.8.2 -> 1.8.2 (latest version 1.9.0)
+- SwiftLint 0.41.0 -> 0.46.2 (latest version 0.46.2)
+- Swinject 2.8.0 -> 2.8.0 (latest version 2.8.1)
+- XMLMapper 1.6.1 -> 2.0.0 (latest version 2.0.0)"
     local SEARCH_STRING="Analyzing dependencies"
     local STATS=${OUTPUT#*$SEARCH_STRING}
 
